e5a191108ee5569d87830af0902140673a2fc19a
mnhkhadang <159155420+mnhkhadang@users.noreply.github.com>
2025-06-17 14:34:05 +0700

    XONG TEST

--- Lab_2/api.py
+++ Lab_2/api.py
@@ -84,4 +84,4 @@ def playfair_decrypt():
 
 #main function
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000,debug=True)
+    app.run(host="127.0.0.1", port=5001, debug=True, use_reloader=False)
